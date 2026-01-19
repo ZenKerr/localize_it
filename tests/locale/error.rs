a009970a0b8b1b09d3cbc039aa4647dd68ba651f
Sarnitskii Ivan <ivan.sarnitskiy@mail.ru>
2026-01-19 22:17:58 +0300

    Change static to const type, for match the expression! macro

--- tests/locale/error.rs
+++ tests/locale/error.rs
@@ -1,6 +1,6 @@
 use crate::locale::Expression;
 
-pub const COOKIE_BUTTON: Expression = [
+pub static COOKIE_BUTTON: Expression = [
     "Sorry, the button doesn't work",
     "Извините, кнопка не работает",
 ];

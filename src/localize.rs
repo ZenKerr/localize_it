5cf40b1d24733ae038f6e6d6d86bed00c4ac8bec
Sarnitskii Ivan <ivan.sarnitskiy@mail.ru>
2026-01-25 19:45:08 +0300

    Clarify documentation and swap localize! variants

--- src/localize.rs
+++ src/localize.rs
@@ -2,24 +2,26 @@
 ///
 /// # Examples
 ///
+/// Using the global locale initialized with
+/// [`init_locale_with_storage!`](crate::init_locale_with_storage!):
+///
+/// ```rust
+/// localize!(HELLO)
+/// ```
+///
 /// Specify the locale manually:
 ///
 /// ```rust
 /// localize!(HELLO, Locale::EN)
 /// ```
-///
-/// Using the global locale (works only with `init_locale_with_storage`):
-///
-/// ```rust
-/// localize!(HELLO)
-/// ```
+/// Locale defined via [`init_locale!`](crate::init_locale!).
 #[macro_export]
 macro_rules! localize {
-    ($expression: path, $locale: expr) => {
-        $expression[$locale as usize]
-    };
-
     ($expression: path) => {
         $expression[get_locale() as usize]
+    };
+
+    ($expression: path, $locale: expr) => {
+        $expression[$locale as usize]
     };
 }

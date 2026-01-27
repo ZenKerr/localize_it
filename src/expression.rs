2e5722d99bd24713aadb14e782c56f100ab4657d
Sarnitskii Ivan <ivan.sarnitskiy@mail.ru>
2026-01-27 12:56:49 +0300

    Separate macros into individual files

--- src/expression.rs
+++ src/expression.rs
@@ -37,32 +37,3 @@ macro_rules! expression {
         };
     };
 }
-
-/// Initializes a localized expressions.
-///
-/// For a single expression, you can use [`expression!`](crate::expression!).
-///
-/// # Examples
-///
-/// ```rust
-/// expressions!(
-///     HELLO => {
-///         EN: "Hello",
-///         RU: "Привет",
-///     },
-///     BYE => {
-///         EN: "Bye",
-///         RU: "Пока",
-///     },
-/// );
-/// ```
-/// `EN` and `RU` are codes defined during `Locale` initialization via
-/// [`init_locale!`](crate::init_locale!).
-#[macro_export]
-macro_rules! expressions {
-    ($($name: ident => {$($lang: ident: $expression: expr),+ $(,)?}),+ $(,)?) => {
-        $(
-            localize_it::expression!($name => {$($lang: $expression),+});
-        )+
-    };
-}

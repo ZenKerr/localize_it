6131da028022f5ca0d28adca0ad9d550343002a3
Sarnitskii Ivan <ivan.sarnitskiy@mail.ru>
2026-01-31 20:58:22 +0300

    Add Locale variants label support

--- tests/locale.rs
+++ tests/locale.rs
@@ -6,11 +6,20 @@ use common::Locale;
 fn tests() {
     assert_eq!(Locale::COUNT, 2);
     assert_eq!(Locale::VARIANTS, [Locale::EN, Locale::RU]);
+    assert_eq!(Locale::LABELS, ["EN", "RU"]);
     assert_eq!(Locale::DEFAULT, Locale::default());
 
     assert_eq!(format!("{}", Locale::EN), "EN");
 
-    for (i, variant) in Locale::iter().enumerate() {
+    for (i, pair) in Locale::iter().enumerate() {
+        assert_eq!(pair, (Locale::VARIANTS[i], Locale::LABELS[i]));
+    }
+
+    for (i, variant) in Locale::iter_variants().enumerate() {
         assert_eq!(variant, Locale::VARIANTS[i]);
+    }
+
+    for (i, label) in Locale::iter_labels().enumerate() {
+        assert_eq!(label, Locale::LABELS[i]);
     }
 }

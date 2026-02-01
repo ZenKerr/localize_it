8ed74b655cc3252f57c6f89dacf162a3f88cbbcd
Sarnitskii Ivan <ivan.sarnitskiy@mail.ru>
2026-02-01 14:31:17 +0300

    Add benchmark

---
+++ benches/lib.rs
@@ -0,0 +1,43 @@
+mod common;
+
+use common::{get_locale, Locale, ENTER_YOU_NAME, HELLO};
+use core::hint::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+use localize_it::localize;
+
+const STR: &'static str = ENTER_YOU_NAME[0];
+const NAME: &'static str = black_box("Ivan");
+const LOCALE: Locale = black_box(Locale::EN);
+
+fn default(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("bench");
+
+    group.bench_function("str", |bencher| bencher.iter(|| STR));
+
+    group.bench_function("format", |bencher| {
+        bencher.iter(|| format!("Hello, {NAME}!"))
+    });
+
+    group.finish();
+}
+
+fn localize_it(criterion: &mut Criterion) {
+    let mut group = criterion.benchmark_group("bench");
+
+    group.bench_function("expression", |bencher| {
+        bencher.iter(|| localize!(ENTER_YOU_NAME))
+    });
+
+    group.bench_function("expression_manual_locale", |bencher| {
+        bencher.iter(|| localize!(ENTER_YOU_NAME, LOCALE))
+    });
+
+    group.bench_function("callable_expression", |bencher| {
+        bencher.iter(|| localize!(HELLO as (NAME)))
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, default, localize_it);
+criterion_main!(benches);
